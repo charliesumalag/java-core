ed43cec77febe173c0f5bc4dd4db0463a46a756e
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-11-14 14:06:53 +0800

    Removed .class files from tracking

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
+    int globalVar = 20;
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int age = 0;
+        Main mainClass = new Main();
+        System.out.println(mainClass.globalVar);
+        age = 10;
+        System.out.println("you are " + age + " years old." );
     }
 }
