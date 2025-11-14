cc3ddee65fa354a4514fc6331f777517d5943ac2
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-11-14 14:24:20 +0800

    data types priimitives

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
 class Main {
     int globalVar = 20;
     public static void main(String[] args) {
-        int age; //creating an variable. variable is an storage of an values.
-        age = 10;
-        System.out.println("you are " + age + " years old." );
+        //Data Types
+        //integer types
+        byte aSingleByte = 100; //-128 to 127
+        short aSmallNumber = 2000; //-32,768 to 32,768
+        int anInteger = 214749323; //-2147483648 to -2147483648
+        long aLargeNumber = 232332232; //this is for large umbers
+
+
+        //decimal types
+        double aDoube = 1.792929;
+        float aFlot =  3.4025F;
+
+        //booleans
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char middleName = 'A';
+
+        System.out.println("You're middle name is:   " + middleName);
     }
 }
